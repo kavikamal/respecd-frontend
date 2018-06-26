f94cfc17cb7ee6fc3cf0225c6fe3db3d52297983
Kavitha Kamal <kavithakamal@gmail.com>
2018-06-26 12:52:34 -0400

    Adding seperate folder for actions reducers and components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          Hello This is the home page for re-spec'd  
+      </div>
+    );
+  }
+}
+
+export default App;
