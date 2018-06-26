59a7bec72f7f4cc2231d11784d0e4518255ae415
Peter Marsh <p.marsh@mac.com>
2018-06-26 16:24:00 -0400

    added loginregister

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
+import LoginRegister from './LoginRegister';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          Hello This is the home page for re-spec'd  
-      </div>
+      <React.Fragment>
+        <LoginRegister />
+      </React.Fragment>
     );
   }
 }
