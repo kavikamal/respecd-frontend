c978cbfba6505993e35ae81991c2c3a310af57ca
Ryan <jragard@gmail.com>
2018-06-27 13:34:46 -0400

    gettin' up to date

--- src/components/Glasses.jsx
+++ src/components/Glasses.jsx
@@ -3,8 +3,7 @@ import '../App.css';
 import './App.jsx';
 import { Button, Form, Divider, Container, Header, Sidebar, Segment, Menu, Icon, Image } from 'semantic-ui-react';
 import Listing from './Listing.jsx';
-import SideNav from './Sidebar.jsx';
-import SidebarExampleSidebar from './newSidebar.jsx';
+
 
 class Glasses extends Component {
 
