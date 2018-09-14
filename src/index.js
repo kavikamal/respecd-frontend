9b2201bd102ba9af58018ebabf3fce741d4317d2
Kavitha <32585598+kavikamal@users.noreply.github.com>
2018-09-14 16:40:55 -0400

    Update index.js
    Fixed a bug. Now the app will open in mobile browser also and browser without redux tools

--- src/index.js
+++ src/index.js
@@ -11,10 +11,13 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 
-const store = createStore(reducer,
-  compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+// const store = createStore(reducer,
+//   compose(applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore( reducer, composeEnhancers( applyMiddleware( thunk ) ) );
 
 
 ReactDOM.render(
